dd40176373db29d49e23e39e4ed1639df65ef8d1
Geordani Machado <74798854+Geordani-Machado@users.noreply.github.com>
2021-08-17 17:41:45 -0300

    Integração Braip - Mongo

--- models/user.js
+++ models/user.js
@@ -15,10 +15,10 @@ const UserSchema = new mongoose.Schema({
      required: false,
   },
   saldo: {
-     type: String,
-     required: false,
+    type: String,
+    required: false,
     default: '20,00',
-  },
+ },
   password: {
     type: String,
     required: true,
@@ -34,6 +34,38 @@ const UserSchema = new mongoose.Schema({
   date: {
     type: Date,
     default: Date.now,
+  },
+  braipCBSB: {
+    type: String,
+    default: 'Olá [ClientNome] sua compra no Boleto foi realizada com sucesso.',
+  },
+  braipCBSP: {
+    type: String,
+    default: 'Olá [ClientNome] sua compra no PIX foi realizada com sucesso.',
+  },
+  braipBG01: {
+    type: String,
+    default: 'Olá [ClientNome] O boleto [Boleto] foi gerado em nossa loja.',
+  },
+  braipBG02: {
+    type: String,
+    default: 'Olá [ClientNome] O boleto [Boleto] foi gerado em nossa loja.',
+  },
+  braipBG03: {
+    type: String,
+    default: 'Olá [ClientNome] se você aind atem interesse em nosso produto, não se esqueca de pagar o boleto. [Boleto]',
+  },
+  braipBG04: {
+    type: String,
+    default: 'Olá [ClientNome] O boleto [Boleto] foi gerado em nossa loja.',
+  },
+  braipBG05: {
+    type: String,
+    default: 'Olá [ClientNome] O boleto [Boleto] foi gerado em nossa loja.',
+  },
+  braipBG06: {
+    type: String,
+    default: 'Olá [ClientNome], Hoje é o ultimo dia para pagar o boleto [boleto], e aproveitar essa super oportunidade de 10% somente para o dia de hoje!',
   },
 });
 
